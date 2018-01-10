811f4c8bfcdc9b4757eaed9800b0a8bd30d28990
Nguyen Chi Cong <congnc.if@gmail.com>
2018-01-10 11:47:59 +0700

    Update viewcontroller + show

--- SiFUtilities/Classes/UIViewController+Show.swift
+++ SiFUtilities/Classes/UIViewController+Show.swift
@@ -7,27 +7,67 @@
 //
 
 import Foundation
-
 import UIKit
 
 extension UIViewController {
-    public func show(on baseViewController: UIViewController) {
+    public func show(on baseViewController: UIViewController,
+                     animated: Bool = true,
+                     completion: (() -> Void)? = nil) {
         if let navigation = baseViewController as? UINavigationController {
-            navigation.pushViewController(self, animated: true)
+            navigation.pushViewController(self, animated: animated)
+            if let completion = completion {
+                DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.25, execute: completion)
+            }
         } else {
-            baseViewController.present(self, animated: true, completion: nil)
+            baseViewController.present(self, animated: animated, completion: completion)
         }
     }
     
-    public func show(viewController: UIViewController, from baseviewController: UIViewController? = nil) {
+    public func show(viewController: UIViewController,
+                     from baseviewController: UIViewController? = nil,
+                     animated: Bool = true,
+                     completion: (() -> Void)? = nil) {
         guard let base = baseviewController else {
             if let navigation = self.navigationController {
-                viewController.show(on: navigation)
+                viewController.show(on: navigation, animated: animated, completion: completion)
             } else {
-                viewController.show(on: self)
+                viewController.show(on: self, animated: animated, completion: completion)
             }
             return
         }
-        viewController.show(on: base)
+        viewController.show(on: base, animated: animated, completion: completion)
+    }
+    
+    public func backToPrevious(animated: Bool = true, completion: (() -> Void)? = nil) {
+        if let navigation = self.navigationController {
+            if navigation.viewControllers.first != self {
+                navigation.popViewController(animated: animated)
+                if let completion = completion {
+                    DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.25, execute: completion)
+                }
+            } else {
+                if let _ = navigation.presentingViewController {
+                    navigation.dismiss(animated: animated, completion: completion)
+                } else {
+                    print("Previous page not found")
+                }
+            }
+        } else if let _ = self.presentingViewController {
+            dismiss(animated: animated, completion: completion)
+        } else {
+            print("Previous page not found")
+        }
+    }
+    
+    public func forceDismiss(animated: Bool = true, completion: (() -> Void)? = nil) {
+        if let _ = self.presentingViewController {
+            dismiss(animated: animated, completion: completion)
+        } else if let navigation = navigationController, let _ = navigation.presentingViewController {
+            navigation.dismiss(animated: animated, completion: completion)
+        } else if let presented = self.presentedViewController {
+            presented.dismiss(animated: animated, completion: completion)
+        } else {
+            print("Presenting page not found")
+        }
     }
 }
