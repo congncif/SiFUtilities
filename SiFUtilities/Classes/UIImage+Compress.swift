b5e228953bb04ffb06bf85f4013d643f92a087d0
NGUYEN CHI CONG <congnc.if@gmail.com>
2018-04-05 23:36:12 +0700

    Reverse image compress

--- SiFUtilities/Classes/UIImage+Compress.swift
+++ SiFUtilities/Classes/UIImage+Compress.swift
@@ -32,3 +32,28 @@ extension UIImage {
         return UIImageJPEGRepresentation(self, quality.rawValue)
     }
 }
+
+extension UIImage {
+    public func compress(ratio: CGFloat, minLength: CGFloat = 100) -> UIImage? {
+        let size = self.size
+        let widthRatio = minLength / size.width
+        let heightRatio = minLength / size.height
+        let finalRatio = max(widthRatio, heightRatio, ratio)
+        return compress(ratio: finalRatio)
+    }
+
+    public func withMinLength(_ minLength: CGFloat) -> UIImage? {
+        let widthRatio = minLength / size.width
+        let heightRatio = minLength / size.height
+        let finalRatio = widthRatio > heightRatio ? widthRatio : heightRatio
+        return compress(ratio: finalRatio)
+    }
+
+    public func compress(ratio: CGFloat) -> UIImage? {
+        let size = self.size
+        let compressSize = CGSize(width: size.width * ratio, height: size.height * ratio)
+        let imageView = UIImageView(frame: CGRect(origin: CGPoint.zero, size: compressSize))
+        imageView.image = self
+        return imageView.takeImage()
+    }
+}
