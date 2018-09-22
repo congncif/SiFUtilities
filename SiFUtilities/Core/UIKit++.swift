9b081bebede086ec40bf97cd193a2ea8b06f35c1
NGUYEN CHI CONG <congnc.if@gmail.com>
2018-09-22 12:14:07 +0700

    Fixed build

--- SiFUtilities/Core/UIKit++.swift
+++ SiFUtilities/Core/UIKit++.swift
@@ -45,22 +45,3 @@ extension UIColor {
         return UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: alpha)
     }
 }
-
-
-//MARK: - UIImage
-
-extension UIImage {
-    open class func image(color: UIColor, size: CGSize = CGSize(width: 1, height: 1)) -> UIImage? {
-        let rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
-        UIGraphicsBeginImageContext(rect.size)
-        let context = UIGraphicsGetCurrentContext()
-        
-        context?.setFillColor(color.cgColor)
-        context?.fill(rect)
-        
-        let image = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        
-        return image
-    }
-}
