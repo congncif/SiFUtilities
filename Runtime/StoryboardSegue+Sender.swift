8d5b20fd1859c7933f6338692c87a65b2757a93d
CONG NGUYEN CHI <congnc.if@gmail.com>
2019-09-15 17:58:11 +0700

    Make UIStoryboardSegue to be able keep passed data

---
+++ Runtime/StoryboardSegue+Sender.swift
@@ -0,0 +1,23 @@
+//
+//  StoryboardSegue+Sender.swift
+//  
+//
+//  Created by NGUYEN CHI CONG on 9/15/19.
+//
+
+import Foundation
+import UIKit
+
+private var senderKey: UInt8 = 1
+
+extension UIStoryboardSegue: AssociatedObject {
+    public var sender: Any? {
+        set {
+            setAssociatedObject(key: &senderKey, value: newValue)
+        }
+
+        get {
+            getAssociatedObject(key: &senderKey)
+        }
+    }
+}
