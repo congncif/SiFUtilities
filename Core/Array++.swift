fc2eb5c036e154a2f22bf29b3c84f7826cfd7885
CONG NGUYEN CHI <congnc.if@gmail.com>
2020-04-15 22:13:29 +0700

    Add sequence unique func

--- Core/Array++.swift
+++ Core/Array++.swift
@@ -66,3 +66,10 @@ extension Collection {
         return indices.contains(index) ? self[index] : nil
     }
 }
+
+extension Sequence where Iterator.Element: Hashable {
+    public func unique() -> [Iterator.Element] {
+        var seen: Set<Iterator.Element> = []
+        return filter { seen.insert($0).inserted }
+    }
+}
