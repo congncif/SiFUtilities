d40bc70565838f97b99102f6e1787b86b3b0b701
NGUYEN CHI CONG <congnc.if@gmail.com>
2021-01-15 11:47:47 +0700

    [UIViewController+Attach] Fix type infer when get attachedObject

--- UIKit/UIViewController+Attach.swift
+++ UIKit/UIViewController+Attach.swift
@@ -12,36 +12,36 @@ import UIKit
 
 final class WrappedController: UIViewController {
     let associatedObject: AnyObject
-    
+
     init(associatedObject: AnyObject) {
         self.associatedObject = associatedObject
         super.init(nibName: nil, bundle: nil)
     }
-    
+
     @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
 
-extension UIViewController {
-    public func attachObject(_ object: AnyObject) {
+public extension UIViewController {
+    func attachObject(_ object: AnyObject) {
         let notAttached = children.allSatisfy {
             ($0 as? WrappedController)?.associatedObject !== object
         }
-        
+
         guard notAttached else {
             #if DEBUG
             print("⚠️ \(object) was attached to \(self)!!!")
             #endif
             return
         }
-        
+
         let controller = WrappedController(associatedObject: object)
         addChild(controller)
     }
-    
-    public func attachedObject<Object: AnyObject>(identifier: ObjectIdentifier? = nil) -> Object? {
+
+    func attachedObject<Object: AnyObject>(identifier: ObjectIdentifier? = nil) -> Object? {
         children.compactMap {
             let object = ($0 as? WrappedController)?.associatedObject as? Object
             if let id = identifier, let existObject = object, ObjectIdentifier(existObject) == id {
@@ -51,12 +51,31 @@ extension UIViewController {
             }
         }.first
     }
-    
-    public func detachObject(_ object: AnyObject) {
+
+    func attachedObjects<Object>() -> [Object] {
+        children.compactMap {
+            ($0 as? WrappedController)?.associatedObject as? Object
+        }
+    }
+
+    func firstAttachedObject<Object>() -> Object? {
+        attachedObjects().first
+    }
+
+    func lastAttachedObject<Object>() -> Object? {
+        attachedObjects().last
+    }
+
+    func detachObject(_ object: AnyObject) {
         children.filter {
             ($0 as? WrappedController)?.associatedObject === object
+        }.forEach {
+            $0.removeFromParent()
         }
-        .forEach {
+    }
+
+    func detachAllObjects() {
+        children.filter { $0 is WrappedController }.forEach {
             $0.removeFromParent()
         }
     }
