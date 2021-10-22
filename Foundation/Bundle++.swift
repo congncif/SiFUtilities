223819700cf17558f49bd454b14af5057104f2f2
NGUYEN CHI CONG <congnc.if@gmail.com>
2021-10-22 12:29:46 +0700

    Update Bundle extensions

--- Foundation/Bundle++.swift
+++ Foundation/Bundle++.swift
@@ -7,30 +7,12 @@
 
 import Foundation
 
-extension Bundle {
-    public class var current: Bundle {
-        let caller = Thread.callStackReturnAddresses[1]
-        
-        if let bundle = _cache.object(forKey: caller) {
-            return bundle
-        }
-        
-        var info = Dl_info(dli_fname: "", dli_fbase: nil, dli_sname: "", dli_saddr: nil)
-        dladdr(caller.pointerValue, &info)
-        let imagePath = String(cString: info.dli_fname)
-        
-        for bundle in Bundle.allBundles + Bundle.allFrameworks {
-            if let execPath = bundle.executableURL?.resolvingSymlinksInPath().path,
-                imagePath == execPath {
-                _cache.setObject(bundle, forKey: caller)
-                return bundle
-            }
-        }
-        #if DEBUG
-        print("Bundle not found for caller \"\(String(cString: info.dli_sname))\"")
-        #endif
-        return Bundle.main
+public extension Bundle {
+    static var appVersion: String? {
+        Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String
     }
-    
-    private static let _cache = NSCache<NSNumber, Bundle>()
+
+    static var buildNumber: String? {
+        Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as? String
+    }
 }
